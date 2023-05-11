5c63680211b05e635806f25e941d3f77a778b82f
rbeniten <rbeniten@emeal.nttdata.com>
2023-05-11 08:47:15 +0200

    Updated

--- taller1/src/taller1/Main.java
+++ taller1/src/taller1/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		List<Integer> numbers = List.of(18,6,4,15,55,78,12,9,8);
 		
-		List<Integer> numbersUp10 = numbers.stream().filter(number -> number > 10).collect(Collectors.toList());
+		Long numbersUp10 = numbers.stream().filter(number -> number > 10).count();
 		
 		System.out.println(numbersUp10);
 	}
